454c29fd5ff25f5e039050bd3d5910713917a23e
Hiren <hiren.patel@scanbuy.com>
2021-10-11 18:30:03 -0400

    fix: fix commit in newBeta

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ release tries:
 #fix: a fix # => this should release v1.0.1 
 * feat: first feature \n\n BREAKING CHANGE: it breaks something # => this should create next beta release trying again3
 * created newBeta branch
+* fix: fix commit in newBeta
 */
